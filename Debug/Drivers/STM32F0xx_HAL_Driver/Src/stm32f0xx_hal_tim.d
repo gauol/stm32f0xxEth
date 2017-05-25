4d0d25083aa8596be24d94289556393ce08c993d
Gauol <pgalczyk@gmail.com>
2017-05-25 18:59:06 +0200

    dziala SPI
    dziala spi

---
+++ Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.d
@@ -0,0 +1,86 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Inc/stm32f0xx_hal_conf.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Inc/main.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x6.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/core_cm0.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi_ex.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Inc/stm32f0xx_hal_conf.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Inc/main.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x6.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/core_cm0.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_spi_ex.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+D:/Dropbox/kody/praca/stm32work/stm32f0xxEth/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
